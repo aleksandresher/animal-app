3b87d02ddfe5cb8ca918ff495da1225bc102c2f3
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-03-17 21:08:18 +0400

    User Page. fetch one user. infinity scroll

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -1,4 +1,57 @@
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../context/UserContext";
+import { useContext, useEffect, useState, useRef } from "react";
+
 function User() {
+  const { value, setValue } = useContext(UserContext);
+  const [users, setUsers] = useState();
+  const [error, setError] = useState();
+  const [friends, setFriends] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  const navigate = useNavigate();
+  const elementRef = useRef(null);
+
+  function navigateAndId(userId) {
+    navigate(`/user/${userId}`);
+    setValue({ id: userId });
+    getMore();
+  }
+
+  function onIntersection(entries) {
+    const firstEntry = entries[0];
+    if (firstEntry.isIntersecting && hasMore) {
+      fetchList();
+    }
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersection);
+    if (observer && elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+    return () => {
+      if (observer) {
+        observer.disconnect();
+      }
+    };
+  }, [friends]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${value.id}`
+      )
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, [value]);
+
   return <div></div>;
 }
 
