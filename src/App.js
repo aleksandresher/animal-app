a923025cc0cb9c14132e35cb423c9435e4730631
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-03-17 19:24:14 +0400

    Provide Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Home from "./Pages/Home";
 import User from "./Pages/User";
 import { Routes, Route } from "react-router-dom";
+import UserContext from "./context/UserContext";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState({ id: 1 });
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/user" element={<User />} />
-      </Routes>
+      <UserContext.Provider value={{ value, setValue }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
